305e3fcd01b7131c5c6740c3ddc3e5134bf32767
Alexander Heimbuch <github@heimbu.ch>
2019-05-12 08:34:50 +0200

    dev bump

--- apps/preview/src/app.js
+++ apps/preview/src/app.js
@@ -3,7 +3,7 @@ import App from './App'
 
 // export a factory function for creating fresh app, router and store
 // instances
-export function createApp () {
+export function createApp() {
   const app = new Vue({
     // the root instance simply renders the App component.
     render: h => h(App)
