3da15dddeb2e9575fce572b2ec465ca4c9326416
Alexander Heimbuch <github@heimbu.ch>
2019-05-25 18:58:27 +0200

    dev bump

--- apps/preview/src/components/Header.vue
+++ apps/preview/src/components/Header.vue
@@ -1,19 +1,27 @@
 <template>
   <header class="header">
-    <nav class="navigation">
-      <router-link to="/" exact class="navigation-link">
-        Home
-      </router-link>
-      <router-link to="/episodes" exact class="navigation-link">
-        Episodes
-      </router-link>
-      <router-link to="/subscribe" exact class="navigation-link">
-        Subscribe
-      </router-link>
-      <router-link to="/about" exact class="navigation-link">
-        About
-      </router-link>
-    </nav>
+    <div class="header-wrapper">
+      <div class="navigation-wrapper">
+        <nav class="navigation">
+          <router-link to="/" exact class="navigation-link">
+            Home
+          </router-link>
+          <router-link to="/episodes" exact class="navigation-link">
+            Episodes
+          </router-link>
+          <router-link to="/subscribe" exact class="navigation-link">
+            Subscribe
+          </router-link>
+          <router-link to="/about" exact class="navigation-link">
+            About
+          </router-link>
+        </nav>
+      </div>
+
+      <div class="search-wrapper">
+        <input type="text" class="search-input" placeholder="Search" />
+      </div>
+    </div>
   </header>
 </template>
 
@@ -22,20 +30,72 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+@import '../theme/mixins';
 .header {
   display: flex;
   width: 100%;
-  justify-content: center;
+  border-bottom: 1px solid v(border);
+  padding: 13px 35px;
+}
+
+.header-wrapper {
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  max-width: 1217px;
+}
+
+.navigation-wrapper {
+  align-items: center;
+  display: flex;
+  flex: 1 0 auto;
+  width: 80%;
+}
+
+.search-wrapper {
+  width: 20%;
+
+  .search-input {
+    background: rgba(16, 24, 32, 0.05);
+    border: none;
+    color: #101820;
+    font-size: 13px;
+    font-weight: 500;
+    height: 30px;
+    line-height: 1;
+    min-width: 160px;
+    padding: 10px;
+  }
 }
 
 .navigation {
   padding: 10px;
   font-size: 1.1em;
-  color: #878b8f;
+  flex: 1 0 auto;
+  flex-wrap: wrap;
 
   .navigation-link {
     margin: 0 10px;
-    padding: 6px 0 4px;
+    padding: 4px 4px;
+    color: v(nav-color);
+    position: relative;
+  }
+
+  .navigation-link:hover,
+  .router-link-active {
+    color: v(color);
+
+    &:before {
+      position: absolute;
+      right: 0;
+      bottom: -2px;
+      left: 0;
+      content: '';
+      background: #e6e5e6;
+      height: 3px;
+      width: 100%;
+    }
   }
 }
 </style>
